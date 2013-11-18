e74746cef92009b3cae910a6284266abcbfedbbe
= <wufufufu@gmail.com>
2013-11-18 18:48:38 -0500

    Added jQuery map. Fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo mysql -uroot -e "create database 'mypseudo'"
+sudo echo "create database mypseudo" | mysql
 sudo mysql mypseudo < ./install/mypseudo.sql
 sudo cp ./install/mypseudo /etc/init.d/mypseudo
 sudo chmod 755 /etc/init.d/mypseudo
